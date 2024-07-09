3f315d70d993f67afc3d0368b1a858289b5a7472
Shivansh Goel <112273961+Shivansh175@users.noreply.github.com>
2024-07-09 19:21:26 +0530

    Constants.js_added

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const API_KEY = "5efc2f8d2350ae35d0601cd4c42bd8f7";
+const IMAGE_RAINY = "https://plus.unsplash.com/premium_photo-1671229652411-4468b946b787?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFpbnl8ZW58MHx8MHx8fDA%3D";
+
+export {API_KEY, IMAGE_RAINY};
